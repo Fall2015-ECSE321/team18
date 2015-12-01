39d0e5ecaf1368a7320d2e2ca7d18c6847c2e780
MatBurd <mpb0628@gmail.com>
2015-12-01 12:05:14 -0500

    Added javadoc comments to Goalie.Java

--- src/soccer/Goalie.java
+++ src/soccer/Goalie.java
@@ -1,14 +1,27 @@
 package soccer;
 
+
+/** The Goalie object extends the Player object
+ * 
+ * @author Team 18
+ *
+ */
 public class Goalie extends Player{
 
 	private int saves;
 	private int goalsAllowed;
 
+	/** Constructor method: initialize Goalie 
+	 * 
+	 * @param firstName		The Goalie's first name
+	 * @param lastName		The Goalie's last name
+	 * @param number		The Goalie's player number
+	 */
 	public Goalie(String firstName, String lastName, int number) {
 		super(firstName, lastName, number);
 	}
 
+	
 	public int getSaves() {
 		return saves;
 	}
@@ -17,6 +30,13 @@ public class Goalie extends Player{
 		return goalsAllowed;
 	}
 
+	/** Converts Goalie statistics to string
+	 * 
+	 * @see soccer.Player#toString()
+	 * 
+	 * @return returnString		Lists the Goalie's Player Statistics, as well as the type of Statistic
+	 */
+	
 	public String toString() {
 		String returnString = "";
 		returnString += super.toString();
@@ -25,6 +45,14 @@ public class Goalie extends Player{
 		return returnString;
 	}
 
+	/** Converts a ShotEvent into Goalie statistics
+	 * 
+	 * @see soccer.Player#applyMatchEvent(soccer.ShotEvent)
+	 * 
+	 * @param event		A ShotEvent Object describing a shot on net
+	 * 
+	 * @return			Returns a String for whether a method succeeded or not
+	 */
 	public String applyMatchEvent(ShotEvent event) {
 		if (this == event.getSubscribedGoalie()) {
 			if (super.getMatchEventsApplied().contains(event)) {
@@ -46,6 +74,14 @@ public class Goalie extends Player{
 		}
 	}
 
+	/** Removes ShotEvent from Goalie statistics
+	 * 
+	 * @see soccer.Player#unapplyMatchEvent(soccer.ShotEvent)
+	 * 
+	 * @param event		A ShotEvent Object describing a shot on net
+	 * 
+	 * @return			Returns a String for whether a method succeeded or not
+	 */
 	public String unapplyMatchEvent(ShotEvent event) {
 		if (this == event.getSubscribedGoalie()) {
 			if (super.getMatchEventsApplied().contains(event)) {
